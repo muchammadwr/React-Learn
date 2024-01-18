bf6d733bb8be32ec2f6535a8845cc34284d53172
muchammadwr <muchammadwr@gmail.com>
2024-01-18 10:50:07 +0700

    Rendering List

--- my-project/src/Pages/products.jsx
+++ my-project/src/Pages/products.jsx
@@ -1,28 +1,48 @@
+/* eslint-disable react/jsx-key */
 import CardProduct from "../components/Fragments/CardProduct";
+
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas,
+    delectus dolores facere tempora veritatis officia reiciendis tempore
+    repellat ab velit ipsa alias exercitationem, perspiciatis et quia
+    enim.`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp 2.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas,
+    delectus dolores facere tempora veritatis officia reiciendis tempore
+    repellat ab velit ipsa alias exercitationem.`,
+  },
+
+  {
+    id: 3,
+    name: "Sepatu Bekas",
+    price: "Rp 500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Ini adalah sepatu bekas yang sangat bagus`,
+  },
+];
 
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas,
-          delectus dolores facere tempora veritatis officia reiciendis tempore
-          repellat ab velit ipsa alias exercitationem, perspiciatis et quia
-          enim. Laudantium, sequi incidunt!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas,
-          delectus dolores facere tempora veritatis officia reiciendis tempore
-          repellat ab velit ipsa alias exercitationem, perspiciatis et quia
-          enim. Laudantium, sequi incidunt!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
